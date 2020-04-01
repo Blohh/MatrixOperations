a2774e5be1935aad73de4428b0c099776e00d0ca
Blohh <52373144+Blohh@users.noreply.github.com>
2020-04-01 12:02:14 +0200

    Remove unneccessary myMaths:: in methods declarations

--- MatrixOperations/Vector.h
+++ MatrixOperations/Vector.h
@@ -10,22 +10,20 @@ namespace myMaths {
 		~Vector();
 		void zeros();
 		void ones();
-		void multiple(const double b);
+
 		Vector& operator=(const Vector& copied);
-		myMaths::Vector operator+(const myMaths::Vector& copied) throw();
-		myMaths::Vector operator-(const myMaths::Vector& copied) throw();
-		myMaths::Vector operator+(const double added);
-		myMaths::Vector operator-(const double subbed);
-		myMaths::Vector operator*(const double subbed);
+		Vector operator+(const Vector& copied) throw();
+		Vector operator-(const Vector& copied) throw();
+		Vector operator+(const double added);
+		Vector operator-(const double subbed);
+		Vector operator*(const double subbed);
 		friend std::ostream& operator<<(std::ostream& stream, const Vector& vt);
 	private:
 		std::vector<double> vector;
-		void add(const myMaths::Vector& b);
-
+		void add(const Vector& b);
 		void add(const double b);
-
-		void sub(const myMaths::Vector& b);
-
+		void sub(const Vector& b);
+		void multiple(const double b);
 		void sub(const double b);
 
 	};

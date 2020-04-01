98e831f39ff8cfa57cb415a6f007b2e7d3f4c265
Blohh <52373144+Blohh@users.noreply.github.com>
2020-04-01 12:01:48 +0200

    Create Matrix class schema

--- MatrixOperations/Matrix.h
+++ MatrixOperations/Matrix.h
@@ -1,26 +1,38 @@
 #pragma once
 #include <vector>
+#include <exception>
+#include <iostream>
 #include "Vector.h"
 namespace myMaths {
 	class Matrix
 	{
+	public:
 		Matrix(int rows, int cols);
 		~Matrix();
-	private:
-		std::vector<std::vector<double>> matrix;
-		Matrix multiple(Matrix A, Matrix B);
-		Vector multiple(Matrix A, Vector b);
-		Matrix multiple(Matrix A, double b);
-		Matrix add(Matrix A, Matrix b);
-		Matrix fowardSubstitution();
-
-	public:
 		Matrix diag();
 		Matrix upperTriangle();
 		Matrix lowerTriangle();
 		void zeros();
 		void ones();
-		
+		Matrix& operator=(const Matrix& copied);
+		Matrix operator+(const Matrix& added) throw();
+		Matrix operator-(const Matrix& subbed) throw();
+		Matrix operator+(const double added);
+		Matrix operator-(const double subbed) throw();
+		Matrix operator*(const double multication);
+		Vector operator*(const Vector& miltiplied) throw();
+		Vector operator/(const Vector& substitution) throw();
+
+	private:
+		std::vector<std::vector<double>> matrix;
+		void add(const Matrix& B);
+		void add(const double b);
+		void sub(const Matrix& B);
+		void sub(const double b);
+		void multiple(const double b);
+		void multiple(const Vector& b);
+		void fowardSubstitution(const Vector& b);
+
 	};
 }
 

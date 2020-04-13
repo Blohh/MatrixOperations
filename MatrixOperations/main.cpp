0b169921753a7c8b14cba2c947fba1e7395d25e4
Blohh <52373144+Blohh@users.noreply.github.com>
2020-04-13 19:46:32 +0200

    Write functions for Ex A, B, C in project

--- MatrixOperations/main.cpp
+++ MatrixOperations/main.cpp
@@ -2,28 +2,45 @@
 #include "Matrix.h"
 #include "LineralEquation.h"
 #include <iostream>
-
+int c = 5, d = 7, f = 5;
+int N = 9 * c * d;
+void exAB();
+void exC();
 int main() {
 	try {
-		/* Ex A */
-		myMaths::Matrix mat = myMaths::LineralEquation::generateEquation(12, -1, -1, 5);
-		myMaths::Vector vt = myMaths::LineralEquation::generateVector(5, 5);
-		std::cout << "Matrix\n" << mat << std::endl << "Vector: \n" << vt << std::endl;
-		myMaths::Vector x = myMaths::LineralEquation::Gauss(mat, vt);
-		/* Ex B */
-		std::cout << "Answer (Gauss): "<< x << std::endl;
-		std::cout << "A*x - b " << mat * x - vt << std::endl;
-		myMaths::Vector x2 = myMaths::LineralEquation::Jacobi(mat, vt);
-		std::cout << "Answer (Jacobi): " << x2 << std::endl;
-		std::cout << "A * x - b " <<  mat * x2 - vt << std::endl;
-		std::cout << "Iterations (Jacobi): " << myMaths::LineralEquation::getJacobiIterations() << std::endl;
-		myMaths::Vector x3 = myMaths::LineralEquation::GaussSeidle(mat, vt);
-		std::cout << "Answer (Gauss-Seidle): " << x3 << std::endl;
-		std::cout << "A * x - b " << mat * x3 - vt << std::endl;
-		std::cout << "Iterations (Gauss-Seidle): " << myMaths::LineralEquation::getGaussSeidleIterations() << std::endl;
+		exAB();
+		exC();
+
 	}
 	catch (const char* msg) {
 		std::cout << "Exception happened!\n" << msg << std::endl;
 	}
 	return 0;
+}
+void exAB() {
+	/* Ex A */
+	myMaths::Matrix mat = myMaths::LineralEquation::generateEquation(12, -1, -1, N);
+	myMaths::Vector vt = myMaths::LineralEquation::generateVector(N, f);
+	/* Ex B */
+	myMaths::Vector x1 = myMaths::LineralEquation::Jacobi(mat, vt);
+	std::cout << "norm(A * x - b) (Jacobi) " << (mat * x1 - vt).norm() << std::endl;
+	std::cout << "Iterations (Jacobi): " << myMaths::LineralEquation::getJacobiIterations() << std::endl;
+	std::cout << "Time (Jacobi): " << myMaths::LineralEquation::getJacobiTime() << std::endl << std::endl;
+	myMaths::Vector x2 = myMaths::LineralEquation::GaussSeidle(mat, vt);
+	std::cout << "norm(A * x - b) (Gauss-Seidle) " << (mat * x2 - vt).norm() << std::endl;
+	std::cout << "Iterations (Gauss-Seidle): " << myMaths::LineralEquation::getGaussSeidleIterations() << std::endl;
+	std::cout << "Time (Gauss-Seidle): " << myMaths::LineralEquation::getGaussSeidleTime() << std::endl << std::endl;
+}
+void exC() {
+
+	myMaths::Matrix mat = myMaths::LineralEquation::generateEquation(3, -1, -1, N);
+	myMaths::Vector vt = myMaths::LineralEquation::generateVector(N, f);
+	myMaths::Vector x1 = myMaths::LineralEquation::Jacobi(mat, vt);
+	std::cout << "norm(A * x - b) (Jacobi) " << (mat * x1 - vt).norm() << std::endl;
+	std::cout << "Iterations (Jacobi): " << myMaths::LineralEquation::getJacobiIterations() << std::endl;
+	std::cout << "Time (Jacobi): " << myMaths::LineralEquation::getJacobiTime() << std::endl << std::endl;
+	myMaths::Vector x2 = myMaths::LineralEquation::GaussSeidle(mat, vt);
+	std::cout << "norm(A * x - b) (Gauss-Seidle) " << (mat * x2 - vt).norm() << std::endl;
+	std::cout << "Iterations (Gauss-Seidle): " << myMaths::LineralEquation::getGaussSeidleIterations() << std::endl;
+	std::cout << "Time (Gauss-Seidle): " << myMaths::LineralEquation::getGaussSeidleTime() << std::endl << std::endl;
 }
